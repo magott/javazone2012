c85e9917d393692b7bacf6d0af132fb75873ab2a
Morten Andersen-Gott <morten.andersengott@gmail.com>
2012-09-04 12:49:09 +0200

    Added calendar example

--- src/main/scala/JavaZonePlan.scala
+++ src/main/scala/JavaZonePlan.scala
@@ -1,13 +1,16 @@
 package javazone12
 import unfiltered.filter.Plan
 import unfiltered.request.{Seg, Path, GET}
-import unfiltered.response.{Html5, Ok}
+import unfiltered.response.{ResponseString, CharContentType, Html5, Ok}
+import org.joda.time.{DateTimeZone, LocalDateTime}
 
 class JavaZonePlan extends Plan {
   def intent = {
     case GET(Path(Seg("javazone" :: Nil))) => Ok ~> Html5(<h1>Hei sveis</h1>)
     case GET(Path(Seg("javazone" :: "speakers" :: "arktekk" :: Nil))) => Ok ~> Html5(speakersTable)
     case GET(Path(Seg(Nil))) => Ok ~> Html5(Snippets.welcomePage)
+
+    case GET(Path(Seg("kaare" :: "reminder" :: Nil))) => CharContentType("text/calendar") ~> ResponseString(kaareCal)
   }
 
   def speakersTable = {
@@ -18,5 +21,14 @@ class JavaZonePlan extends Plan {
         arktekkSpeakers.map(s => <tr><td>{s}</td></tr>)
       }
     </table>
+  }
+
+  def kaareCal = {
+    val start = osloToUtc(new LocalDateTime(2012,9,12,15,40))
+    VCalendar(VEvent("True unfiltered love!","Kaare skravler og demonstrerer Unfiltered!",start,start.plusHours(1),"Sal 5")).feed
+  }
+
+  private def osloToUtc(dateTime: LocalDateTime) = {
+    dateTime.toDateTime(DateTimeZone.forID("Europe/Oslo")).withZone(DateTimeZone.UTC).toLocalDateTime
   }
 }
---
+++ src/main/scala/calendar.scala
@@ -0,0 +1,51 @@
+package javazone12
+import org.joda.time._
+
+case class VEvent(title:String, description:String, start:LocalDateTime, end:LocalDateTime, location:String);
+
+case class VCalendar(vevents: VEvent*) {
+
+
+  private val calendarFormatString = "yyyyMMdd'T'HHmmss'Z"
+
+  def feed = {
+    vevents.foldLeft(start)(_ + "\n" + vevent(_)) + "\n" + end
+  }
+
+  private def vevent(v: VEvent) = {
+    """BEGIN:VEVENT
+      |DTSTART:%s
+      |DTEND:%s
+      |DTSTAMP:%s
+      |LAST-MODIFIED:%s
+      |UID:%s
+      |LOCATION:%s
+      |SUMMARY:%s
+      |DESCRIPTION:%s
+      |END:VEVENT""".stripMargin.format(
+      v.start.toString(calendarFormatString),
+      v.end.toString(calendarFormatString),
+      LocalDateTime.now.toString(calendarFormatString),
+      LocalDateTime.now.toString(calendarFormatString),
+      v.title + "@andersen-gott.com",
+      v.location,
+      v.title,
+      v.description
+    )
+  }
+
+  val start =
+    """BEGIN:VCALENDAR
+      |VERSION:2.0
+      |PRODID:-//andersen-gott.com/demo//NONSGML v1.0//EN
+      |METHOD:PUBLISH
+      |X-WR-CALNAME:JZ12-demo
+      |X-WR-CALDESC:JZ12-demo
+      |X-PUBLISHED-TTL:PT1H""".stripMargin
+
+  val end = "END:VCALENDAR"
+
+
+
+}
+
